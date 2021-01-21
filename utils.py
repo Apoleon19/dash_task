0b2c9795f8586223f4ac889a3cf5e89b11bf3321
Apoleon <poleqqq@gmail.com>
2021-01-21 18:42:48 +0300

    save commit

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import xml.etree.ElementTree as et
+
 import pandas as pd
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -17,6 +18,7 @@ def get_xml_data_file():
             if "area" in node.tag:
                 parsed_area = _parse_area_node(node)
                 prepared_data = _prepare_data(parsed_area)
+                print(prepared_data)
                 return prepared_data
 
 
@@ -47,13 +49,14 @@ def _parse_area_node(node) -> dict:
 
 def _prepare_data(data) -> pd.DataFrame:
     """prepare area data and convert to data frame"""
+    # measuringpoint name, measuringchannel desc, 1...48 periods
     df = pd.DataFrame(data['data'], columns=['name', 'channels'])
     for channel_data in df['channels']:
-        _calculate_consuming(channel_data)
-        break
+        computed_consuming = _calculate_consuming(channel_data)
+        channel_data = computed_consuming
+    return df
 
-
-def _calculate_consuming(channel_data) -> pd.DataFrame:
+def _calculate_consuming(channel_data) -> list:
     """calculate consuming for each measuring point
         01, 03 - input
         02, 04 - output
@@ -66,25 +69,36 @@ def _calculate_consuming(channel_data) -> pd.DataFrame:
         else:
             points['output'].append(periods)
 
-    for _, point in points.items():
-        data = _sum_dict_values(point, 'value')
-        print(data)
+    for point_type, point in points.items():
+        points[point_type] = _compute_dict_values(point, 'value', summary)
+
+    all_points_sum = _compute_dict_values([points['input'], points['output']], 'value', summary)
+    return all_points_sum
 
 
-def _sum_dict_values(data, key) -> list:
-    """sum by 'value' key from measuring point"""
+def _compute_dict_values(data: list, key: str, operation) -> list:
+    """sum by key from measuring point
+       operation - custom functions minus and summary
+    """
     computed_data = list()
-    first_dict = data.pop()
-    second_dict = data.pop()
+    first_dict, second_dict = data
     for index, item in enumerate(first_dict):
         item_two = second_dict[index]
         if item['start'] == item_two['start'] and item['end'] == item_two['end']:
-            items_value_sum = item[key] + item_two[key]
-            item.update({key: items_value_sum})
+            items_operation_result = operation(item[key], item_two[key])
+            item.update({key: items_operation_result})
             computed_data.append(item)
         else:
-            raise Exception(f'Error when sum dict {item} and {item_two}')
+            raise Exception(f'Error when try to {operation} with dict {item} and {item_two} by key "{key}"')
     return computed_data
+
+
+def minus(a, b):
+    return a - b
+
+
+def summary(a, b):
+    return a + b
 
 
 get_xml_data_file()
