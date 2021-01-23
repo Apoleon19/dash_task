7ddc012f2648e1c37f6457affab1317fa7785ecb
Apoleon <poleqqq@gmail.com>
2021-01-23 19:15:46 +0300

    complete test task

--- main.py
+++ main.py
@@ -1,31 +1,94 @@
 import dash
+import dash_core_components as dcc
 import dash_html_components as html
-from utils import get_xml_data_file
-from collections import defaultdict
-
-import dash
 import dash_table
-import pandas as pd
+import plotly.express as px
+from dash.dependencies import Input
+from dash.dependencies import Output
+
+from utils import get_data_from_xml
 
 app = dash.Dash(__name__)
-name, df = get_xml_data_file()
+name, df = get_data_from_xml()
 
-# app.layout = html.Div(className="container", children=[
-#     html.H1(className="container__title", children=name),
-#     html.Div(children="test"),
-#
-#
-# ])
+app.layout = html.Div([
+    html.Div([html.Div(name, style={'textAlign': 'center'}, className='dash-title'),
+              html.Div([
+                  html.Button('Табличный вид', id='btn-change-table', n_clicks=0, className='btn'),
+                  html.Button('Графический вид', id='btn-change-graph', n_clicks=0, className='btn',
+                              style={'marginTop': '5px'})
+              ], className='buttons-box')], className='dash-header'),
 
-#
-
-app.layout = dash_table.DataTable(
-    id='table',
-    # columns=[gen_table_period_columns()],
-    # data=gen_table_period_columns(),
+    html.Div([
+        html.Div(dash_table.DataTable(
+            id='consuming-table',
+            columns=[{"name": i, "id": i} for i in df.columns],
+            data=df.to_dict('records'),
+            style_cell={
+                'whiteSpace': 'normal',
+                'height': 'auto',
+                'border': '1px solid #999999'
+            },
+            style_header={'textAlign': 'center', 'backgroundColor': 'rgb(220, 220, 220)', 'font-weight': 'bold'},
+            style_data={'textAlign': 'center', 'background': ''},
+            style_table={'marginTop': '5px', 'background': 'none'},
+            style_data_conditional=[
+                {'if': {'row_index': 'odd'},
+                 'backgroundColor': '#e0e0e0'},
+                {
+                    'if': {'state': 'selected'},
+                    'backgroundColor': '#c2c2c2',
+                    'border': '1px solid black'
+                }
+            ]
+        ), id='consuming-table-container'),
+        html.Div([
+            dcc.Graph(id='consuming-graph'),
+            dcc.Dropdown(
+                id='dropdown-graph',
+                options=[{'label': f'Период {i}', 'value': i} for i in df.columns[2:]],
+                value=1,
+                searchable=False,
+                placeholder='Выберите период...'
+            ),
+        ], id='consuming-graph-container')
+    ], className='dash-container'),
+], className='consuming-dashboard', style={'overflowX': 'auto'},
 )
 
 
+@app.callback(
+    dash.dependencies.Output('consuming-table-container', 'style'),
+    dash.dependencies.Output('consuming-graph-container', 'style'),
+    Input('btn-change-table', 'n_clicks'),
+    Input('btn-change-graph', 'n_clicks')
+)
+def display_btn(btn1, btn2):
+    triggered_prop = [prop['prop_id'] for prop in dash.callback_context.triggered][0]
+    prepared_styles = list()
+    for btn_class in ('btn-change-table', 'btn-change-graph'):
+        if btn_class in triggered_prop or btn_class == 'btn-change-graph' and triggered_prop == '.':
+            prepared_styles.append({'display': 'flex'})
+        else:
+            prepared_styles.append({'display': 'none'})
+    return prepared_styles
+
+
+@app.callback(
+    Output('consuming-graph', 'figure'),
+    Input('btn-change-graph', 'n_clicks'),
+    Input('dropdown-graph', 'value')
+)
+def table_to_graph(btn_click, select_period):
+    prepared_df = df.groupby(['Точка учета'], as_index=True).sum().T
+    select_period = int(select_period) if select_period else 1
+    selected_data = prepared_df[prepared_df.index.astype('int64') >= select_period]
+
+    fig = px.line(selected_data, x=selected_data.index, y=selected_data.columns, template='plotly_dark')
+    fig.update_layout(xaxis_title='Период', yaxis_title='Значение потребления', hovermode="x")
+    fig.update_traces(mode='lines+markers', hovertemplate="Период: %{x} <br>Потребление: %{y}<extra></extra>", )
+    return fig
+
+
 if __name__ == '__main__':
-    # gen_table_period_columns()
     app.run_server(debug=True)
--- utils.py
+++ utils.py
@@ -7,8 +7,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 
 
-def get_xml_data_file():
-    """Get first xml file in data folder and parse to data frame"""
+def get_data_from_xml() -> tuple:
+    """Get first xml file in data folder and parse name and data frame"""
     data_files = [file for file in os.listdir(DATA_DIR) if '.xml' in file]
     if data_files:
         data_file_path = os.path.join(DATA_DIR, data_files.pop())
@@ -16,13 +16,12 @@ def get_xml_data_file():
         root = tree.getroot()
         for node in root:
             if "area" in node.tag:
-                parsed_area = _parse_area_node(node)
+                name, parsed_area = _parse_area_node(node)
                 prepared_data = _prepare_data(parsed_area)
-                print(prepared_data)
-                return prepared_data
+                return name, prepared_data
 
 
-def _parse_area_node(node) -> dict:
+def _parse_area_node(node) -> tuple:
     """parse data from area node and return name and data"""
     name = node.find("name").text
     measuring_points = node.findall('measuringpoint')
@@ -44,61 +43,23 @@ def _parse_area_node(node) -> dict:
             point_data['channels'].append(channel_data)
 
         points_data.append(point_data)
-    return {'name': name, 'data': points_data}
+    return name, points_data
 
 
 def _prepare_data(data) -> pd.DataFrame:
     """prepare area data and convert to data frame"""
     # measuringpoint name, measuringchannel desc, 1...48 periods
-    df = pd.DataFrame(data['data'], columns=['name', 'channels'])
-    for channel_data in df['channels']:
-        computed_consuming = _calculate_consuming(channel_data)
-        channel_data = computed_consuming
+    prepared_data = []
+    for point in data:
+        for channels in point['channels']:
+            prepared_dict = {'name': point['name'], 'channel_name': channels['desc']}
+            periods = channels['periods']
+            for index, period in enumerate(periods):
+                prepared_dict.update({f'{index+1}': period['value']})
+            prepared_data.append(prepared_dict)
+    df = pd.DataFrame(prepared_data)
+    df.rename(columns={'name': 'Точка учета', 'channel_name': 'Канал'}, inplace=True)
     return df
 
-def _calculate_consuming(channel_data) -> list:
-    """calculate consuming for each measuring point
-        01, 03 - input
-        02, 04 - output
-    """
-    points = {'input': [], 'output': []}
-    for channel in channel_data:
-        periods = channel['periods']
-        if int(channel['code']) % 2:
-            points['input'].append(periods)
-        else:
-            points['output'].append(periods)
 
-    for point_type, point in points.items():
-        points[point_type] = _compute_dict_values(point, 'value', summary)
-
-    all_points_sum = _compute_dict_values([points['input'], points['output']], 'value', summary)
-    return all_points_sum
-
-
-def _compute_dict_values(data: list, key: str, operation) -> list:
-    """sum by key from measuring point
-       operation - custom functions minus and summary
-    """
-    computed_data = list()
-    first_dict, second_dict = data
-    for index, item in enumerate(first_dict):
-        item_two = second_dict[index]
-        if item['start'] == item_two['start'] and item['end'] == item_two['end']:
-            items_operation_result = operation(item[key], item_two[key])
-            item.update({key: items_operation_result})
-            computed_data.append(item)
-        else:
-            raise Exception(f'Error when try to {operation} with dict {item} and {item_two} by key "{key}"')
-    return computed_data
-
-
-def minus(a, b):
-    return a - b
-
-
-def summary(a, b):
-    return a + b
-
-
-get_xml_data_file()
+get_data_from_xml()
